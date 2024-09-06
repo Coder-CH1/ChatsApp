bab9a543044fd4dc68649ff7b104c0152acdaf48
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-09-06 04:05:58 +0100

    added

--- lib/user.dart
+++ lib/user.dart
@@ -1,12 +1,10 @@
 import 'package:chatsapp/resusable_widgets/custom_color.dart';
 import 'package:flutter/material.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
-
 import 'home.dart';
 
 class UserRegistration extends StatefulWidget {
-  const UserRegistration({Key? key}) : super(key: key);
+  const UserRegistration({super.key});
 
   @override
   State<UserRegistration> createState() => _UserRegistrationState();
@@ -15,6 +13,21 @@ class UserRegistration extends StatefulWidget {
 class _UserRegistrationState extends State<UserRegistration> {
   String initialCountry = '';
   PhoneNumber number = PhoneNumber(isoCode: '');
+  final GlobalKey<FormState> _form = GlobalKey<FormState>();
+  final TextEditingController phoneNumber = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  final TextEditingController confirmPassword = TextEditingController();
+
+  moveToChatPage(BuildContext context) {
+    if (_form.currentState!.validate()) {
+      setState(() {
+        Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const Home()),
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,79 +44,139 @@ class _UserRegistrationState extends State<UserRegistration> {
         decoration: BoxDecoration(
           gradient: CustomColor.multiColors,
         ),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 100, left: 20, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Connect\nfriends\neasily &\nquickly', style:
-                    TextStyle(
-                        color: Colors.white,
-                        fontSize: 45,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Caros',
-                    ),
-                      textAlign: TextAlign.left,
-                    ),
-                    Text('Our chat app is the perfect way to stay\nconnected with friens and family', style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black54,
-                      fontFamily: 'Caros',
-                    ),
-                      textAlign: TextAlign.left,
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Text('Sign in with Phone Number', style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    ),
-                    IntlPhoneField(
-                      //focusNode: focusNode,
-                      //dropdownIconPosition: IconPosition.trailing,
-                      decoration: const InputDecoration(
-                          labelText: 'Phone number',
-                          hintText: 'phone number'
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 60, left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Connect\nfriends\neasily &\nquickly', style:
+                      TextStyle(
+                          color: Colors.white,
+                          fontSize: 45,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Caros',
                       ),
-                      initialCountryCode: '',
-                      showCountryFlag: false,
-                      languageCode: 'en',
-                      autofocus: false,
-                      onChanged: (phone) {
-                        print(phone.completeNumber);
-                      },
-                    ),
-                  ],
-                ),
-            ),
-          SizedBox(
-           height: 40,
-          ),
-          SizedBox(
-            height: 48,
-            width: 327,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFFFE81D)
+                        textAlign: TextAlign.left,
+                      ),
+                      Text('Our chat app is the perfect way to stay\nconnected with friens and family', style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black54,
+                        fontFamily: 'Caros',
+                      ),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text('Sign in with Phone Number', style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      ),
+                      TextFormField(
+                        controller: phoneNumber,
+                        keyboardType: TextInputType.phone,
+                        decoration: const InputDecoration(
+                            hintText: 'phone number',
+                          hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                          errorStyle: TextStyle(color: Colors.red),
+                        ),
+                        autofocus: false,
+                        validator: (val) {
+                          if (val == null || val.isEmpty) {
+                           return 'Phone number cannot be empty';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        controller: password,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)
+                          ),
+                          //labelText: 'Password',
+                          hintText: 'Enter your password',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          errorStyle: TextStyle(color: Colors.red),
+                            suffixIcon: Icon(
+                              Icons.visibility,
+                              color: Colors.orangeAccent,
+                            )
+                        ),
+                          validator: (val) {
+                            if(val == null || val.isEmpty) {
+                              return 'Password cannot be empty';
+                            } else if(val.length<6) {
+                              return 'Password length must be at least 6';
+                            }
+                            return null;
+                          }
+                      ),
+                      TextFormField(
+                        controller: confirmPassword,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black)
+                            ),
+                            //labelText: 'Confirm Password',
+                            hintText: 'Enter your password',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          errorStyle: TextStyle(color: Colors.red),
+                            suffixIcon: Icon(
+                              Icons.visibility,
+                              color: Colors.orangeAccent,
+                            )
+                        ),
+                          validator: (val){
+                            if(val == null || val.isEmpty) {
+                              return 'Password cannot be empty';
+                            } else if(val != password.text) {
+                              return 'Password does not Match';
+                            }
+                            return null;
+                          }
+                      ),
+                    ],
+                  ),
               ),
-                onPressed: (){
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const Home()),
-                  );
-                },
-                child: Text('Sign in', style: TextStyle(
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                ))),
-          )
-          ],
+            SizedBox(
+             height: 20,
+            ),
+            SizedBox(
+              height: 48,
+              width: 327,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFFE81D)
+                ),
+                  onPressed: (){
+                   moveToChatPage(context);
+                  },
+                  child: Text('Sign in', style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                  ))),
+            )
+            ],
+          ),
         ),
       ),
     );
