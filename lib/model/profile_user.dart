bfa52fe7cf8ef1b2d26e58a03d19e4e7a48dfd3d
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-09-15 15:36:40 +0100

    added

--- lib/model/profile_user.dart
+++ lib/model/profile_user.dart
@@ -2,13 +2,13 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfileUser {
-  final String user_id;
+  final String id;
   final DateTime created_at;
   final String phone_number;
   final String display_name;
 
   ProfileUser({
-    required this.user_id,
+    required this.id,
     required this.created_at,
     required this.phone_number,
     required this.display_name,
@@ -16,27 +16,29 @@ class ProfileUser {
 
   factory ProfileUser.fromJson(Map<String, dynamic> json) {
     return ProfileUser(
-      user_id: json['userId'],
-      created_at: DateTime.parse(json['createdAt'] as String),
-      phone_number: json['phoneNumber'],
-      display_name: json['displayName'],
+      id: json['id'] as String,
+      created_at: DateTime.parse(json['created_at'] as String),
+      phone_number: json['phone_number'] as String,
+      display_name: json['display_name'] as String,
     );
   }
 
   Map<String, dynamic> toJson() => <String, dynamic>{
-    'userId': user_id,
-    'createdAt': created_at,
-    'phoneNumber': phone_number,
-    'displayName': display_name
+    'id': id,
+    'created_at': created_at,
+    'phone_number': phone_number,
+    'display_name': display_name
   };
 }
 
  Future<ProfileUser?> fetchProfileUser(String userId) async {
-//   final Supabase _supabase;
-//    final response = await _supabase.client
-//        .from('profiles')
-//        .select()
-//        .eq('id, userId')
-//        .single();
-//
+   final response = await Supabase.instance.client
+       .from('profiles')
+       .select()
+       .eq('id', userId)
+       .single();
+
+
+   final profileUser = ProfileUser.fromJson(response as Map<String, dynamic>);
+   return profileUser;
  }
