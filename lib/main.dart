dbea3685c97ff697a78b52a5e19ad6f4466fa33e
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-23 16:39:33 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        useMaterial3: true,
-      ),
-      home: const Auth(),
+      home: Auth(),
     );
   }
 }
